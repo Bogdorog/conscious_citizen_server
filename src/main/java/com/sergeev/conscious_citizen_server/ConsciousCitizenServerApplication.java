1c2632a1a877597c41b59a82467d29f011386ec2
s220090 <220090@edu.psuti.ru>
2026-02-20 22:50:13 +0400

    Создание проекта

---
+++ src/main/java/com/sergeev/conscious_citizen_server/ConsciousCitizenServerApplication.java
@@ -0,0 +1,13 @@
+package com.sergeev.conscious_citizen_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsciousCitizenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsciousCitizenServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sergeev/conscious_citizen_server/ConsciousCitizenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergeev.conscious_citizen_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsciousCitizenServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
