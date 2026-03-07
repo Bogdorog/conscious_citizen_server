000f1c14f4a053cc96538b6b068718bc334eef8d
s220090 <220090@edu.psuti.ru>
2026-03-07 23:47:47 +0400

    Добавлена валидация
    Все запросы теперь валидируются в DTO
    Создана документация в swagger

--- src/main/java/com/sergeev/conscious_citizen_server/user/api/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/sergeev/conscious_citizen_server/user/api/dto/request/UpdateProfileRequest.java
@@ -1,8 +1,32 @@
 package com.sergeev.conscious_citizen_server.user.api.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Schema(description = "Запрос на изменение профиля пользователя")
 public record UpdateProfileRequest(
+        @NotBlank(message = "ФИО не может быть пустым")
+        @Schema(description = "Полное имя пользователя", example = "Сидоров Иван Сергеевич")
         String fullName,
+
+
+        @NotBlank(message = "Телефон не может быть пустым")
+        @Schema(description = "Телефон пользователя", example = "898712356")
         String phone,
+
+        @Email(message = "Некорректный формат email")
+        @NotBlank(message = "Email не может быть пустым")
+        @Schema(description = "Адрес электронной почты пользователя", example = "newuser@study.com")
         String email,
-        String address
+
+        @NotBlank(message = "Адрес не может быть пустым")
+        @Schema(description = "Адрес пользователя", example = "Россия, г. Самара, ул. Солнечная 29")
+        String address,
+
+        @NotBlank(message = "Пароль не может быть пустым")
+        @Size(min = 6, message = "Пароль должен быть не менее 6 символов")
+        @Schema(description = "Пароль пользователя", example = "secure_pass_123")
+        String password
 ) {}
